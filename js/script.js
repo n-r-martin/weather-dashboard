1d149ad2052e09f96a6f86d6662302d6d6f788da
Nick Martin <martin.nicholas.r@gmail.com>
2022-02-01 21:17:21 -0700

    nested api calls are working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 const apiKey = '7f18634c0f8ab52c9c31e83595e4f3b8';
+
 let city = 'denver';
 
-const apiUrl = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+let geoCodeApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
 
 
-
-fetch(apiUrl)
+fetch(geoCodeApiUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+
+    let lat = data[0].lat;
+    let long = data[0].lon;
+    console.log(lat);
+    console.log(long);
+
+    let oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&appid=' + apiKey;
+    
+    fetch(oneCallUrl)
+        .then( function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
   });
+
+
+  
