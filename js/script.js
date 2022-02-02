c00d6920bd403c122c7d7820e0668096ec9b903a
Nick Martin <martin.nicholas.r@gmail.com>
2022-02-01 21:43:48 -0700

    data returning data based on user input

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+const searchBtn = $('#city-search-btn');
+const cityInput = $('#city-input');
+
 const apiKey = '7f18634c0f8ab52c9c31e83595e4f3b8';
 
-let city = 'denver';
 
-let geoCodeApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
-
-
-fetch(geoCodeApiUrl)
+function updateCity(){
+    let city = cityInput.val();
+    let geoCodeApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
+    fetch(geoCodeApiUrl)
   .then(function (response) {
     return response.json();
   })
@@ -27,6 +29,12 @@ fetch(geoCodeApiUrl)
             console.log(data);
         })
   });
+}
+
+  searchBtn.on('click', function (event) {
+      event.preventDefault();
+      updateCity();
+  })
 
 
   
